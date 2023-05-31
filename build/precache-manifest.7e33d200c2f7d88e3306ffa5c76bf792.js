93569cc5e0121a7441c6a6ea3d5ef773f4a5043c
Mohammad Amaan <86742621+mohdamaan570@users.noreply.github.com>
2023-05-31 16:29:45 +0530

    Add files via upload

---
+++ build/precache-manifest.7e33d200c2f7d88e3306ffa5c76bf792.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07acf1737e368d2a0db07ee3a8738e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "204caec3cadef5ca3752",
+    "url": "/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "67f9cf90121d5f2467b7",
+    "url": "/static/css/main.489d73f3.chunk.css"
+  },
+  {
+    "revision": "204caec3cadef5ca3752",
+    "url": "/static/js/2.cf6c9f94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf6c9f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f9cf90121d5f2467b7",
+    "url": "/static/js/main.7e90cc3d.chunk.js"
+  },
+  {
+    "revision": "cb5c3f098ed7fc2988d2",
+    "url": "/static/js/runtime-main.d6b47f06.js"
+  }
+]);
